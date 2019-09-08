a176e3b4cd7b1abcefd5f5abbca6daef6cf7adee
doorisopen <audauddl2@gmail.com>
2019-09-08 16:14:21 +0900

    clock_modify

--- clock.js
+++ clock.js
@@ -9,7 +9,7 @@ function getTime() {
     // mini if
     clockTtile.innerText = `${hours < 10 ? `0${hours}` : hours}:${
         minutes < 10 ? `0${minutes}` : minutes}:${
-            seconds < 9 ? `0${seconds}` : seconds}`;
+            seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
